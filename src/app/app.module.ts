175059f1a75ec9d847b57ffa4bce968e7abfc39b
dvinubius <dvinubiu.narmon@gmail.com>
2018-10-29 00:32:28 +0200

    prepared for heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { ShopMaterialModule } from './shop-material/shop-material.module';
+import { ShoppingCartComponent } from './view/shopping-cart/shopping-cart.component';
+import { HomeComponent } from './view/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ShoppingCartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ShopMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
