2ae41ba34f86f0b6358fdf7ed173067919b6fde0
dvinubius <dvinubiu.narmon@gmail.com>
2018-10-29 00:56:57 +0200

    solved path problem

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/../../dist'));
+app.use(express.static(__dirname + '/dist/shop-project/'));
 
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/../../dist/index.html')
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/shop-project/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
